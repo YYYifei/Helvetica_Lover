8bcc0a199acba6d68c0c2374d4221103009a8465
YYYifei <yy3146@columbia.edu>
2023-03-24 19:48:07 -0400

    Update main2.js

--- exercises/exercises_07/main2.js
+++ exercises/exercises_07/main2.js
@@ -3,6 +3,6 @@ console.log('this works');
 const students = ["Alice", "Han", "Chi Chi", "Brent"]
 
 for(let index = 0; index < students.length; index++){
-    console.log('Name of Student is:' + students[index])
+    console.log('Name of Student is:' + index + " " + students[index])
     //index += 1 //index = index +1 // index++
 }
