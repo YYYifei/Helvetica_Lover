bfaaa6ac9affb04642d74c14d703eee942096506
YYYifei <yy3146@columbia.edu>
2023-03-24 20:15:21 -0400

    Update main2.js

--- exercises/exercises_07/main2.js
+++ exercises/exercises_07/main2.js
@@ -15,6 +15,8 @@ console.log(height) */
 
 if(age < 8){
     console.log("Check out the Merry-Go-Round. You'll love it!")
+} else if (age > 8 && age < 65 && height > 4.5){
+    console.log("Check out the Roller Coaster. It's awesome!")
 } else {
     console.log("Why not enjoy a float down the Lazy River?")
 }
