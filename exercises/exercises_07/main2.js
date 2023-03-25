3b1ed2b818421df77e65120ca925d5c526c54a9d
YYYifei <yy3146@columbia.edu>
2023-03-24 20:09:51 -0400

    Update main2.js

--- exercises/exercises_07/main2.js
+++ exercises/exercises_07/main2.js
@@ -10,5 +10,11 @@ for(let index = 0; index < students.length; index++){
 const age = 25;
 const height = 5;
 
-console.log(age)
-console.log(height)
+/* console.log(age)
+console.log(height) */
+
+if(age < 8){
+    console.log("Check out the Merry-Go-Round. You'll love it!")
+} else {
+    console.log("Why not enjoy a float down the Lazy River?")
+}
