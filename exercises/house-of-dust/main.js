4661ff14194df022861c842c7742da6d3f46e160
YYYifei <yy3146@columbia.edu>
2023-03-28 20:19:40 -0400

    Update main.js

---
+++ exercises/house-of-dust/main.js
@@ -0,0 +1,15 @@
+let array_one = [
+    "a house of wood",
+    "a house of brick",
+    "a house of broken dishes",
+    "a house of discarded clothing",
+    "a house of dust"
+  ];
+  
+  let array_two = [
+    "among other houses",
+    "among small hills",
+    "among high montains",
+    "by a river",
+    "by an abandoned lake"
+  ];
