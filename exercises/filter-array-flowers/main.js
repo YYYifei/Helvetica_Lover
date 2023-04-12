b45a0ec6bdb6be69e8376ed85dab6e3a431fd7fc
YYYifei <yy3146@columbia.edu>
2023-04-11 20:41:54 -0400

    Update main.js

--- exercises/filter-array-flowers/main.js
+++ exercises/filter-array-flowers/main.js
@@ -38,29 +38,30 @@ const flowers = [
 const ul = document.querySelector("ul");
 
 function renderFlowersToPage(results) {
-    // iterate over data set
-    for(let i = 0; i< results.length; i++){
-        //create the list item
-        let listItem = document.createElement('li');
-        //add a class to each item of the resuts
-        listItem.classList.add('card', results[i].color) //red
-        //add the flower name
-        let title = document.createElement('h3')
-        title.textContent = results[i].name //rose
-        //add flower color
-        let color = document.createElement('p')
-        color.classList.add(results[i].color)
-        color.textContent = results[i].color
-        //add flower image
-        let image = document.createElement('img')
-        image.setAttribute('src', results[i].image)
+  // iterate over data set
+  for(let i = 0; i < results.length; i++){
+    // create the list item
+    let listItem = document.createElement('li');
+    // add a class to each item of the results
+    listItem.classList.add('card', results[i].color) // red
+    // add flower name
+    let title = document.createElement('h3')
+    title.textContent = results[i].name // Rose
+    // add flower color
+    let color = document.createElement('p')
+    color.classList.add(results[i].color)
+    color.textContent = results[i].color
 
+    // add flower image
+    let image = document.createElement('img')
+    image.setAttribute('src', results[i].image)
 
-        ul.appendChild(listItem)
-        listItem.appendChild(title)
-        listItem.appendChild(color)
-        listItem.appendChild(image)
-    }
+    ul.appendChild(listItem)
+    listItem.appendChild(title)
+    listItem.appendChild(color)
+    listItem.appendChild(image)
+
+  }
 }
 renderFlowersToPage(flowers);
 
@@ -71,26 +72,26 @@ let cards = document.querySelectorAll(".card");
 
 function filterFn(event) {
   console.log(event.target)
-  if(event.target.classList.contains('fiter-btn')){
-    //select the current active button
-   let activeBtn = filterBtns.querySelector('.active')
-   activeBtn.classList.remove('active')
+  if(event.target.classList.contains('filter-btn')){
+    // select the current active button
+    let activeBtn = filterBtns.querySelector('.active')
+    activeBtn.classList.remove('active')
 
-   //apply the active class to the target
-   event.target.classList.add('active')
+    // apply the active class to the target
+    event.target.classList.add('active')
 
-   const filterValue = event.target.getAttribute('data-filter')
-   for(let i = 0; I < cards.length; i++){
-    if(cards[i].classList.contains(filterValue) || filterValue === 'all'){
+    const filterValue = event.target.getAttribute('data-filter') // yellow | red
+
+    for(let i = 0; i < cards.length; i++){
+      if(cards[i].classList.contains(filterValue) || filterValue === 'all'){
         cards[i].classList.remove('hide')
         cards[i].classList.add('show')
-    } else{
+      } else {
         cards[i].classList.remove('show')
         cards[i].classList.add('hide')
-    }
-   }
-}
-}
+      }
 
-//function filterFn(e) {}
+    }
+  }
+}
 filterBtns.addEventListener("click", filterFn);
